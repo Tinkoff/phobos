de449f994aa6e96d252b31170579822b6c2a1f2f
valentiay <valentiay@yandex.ru>
2019-09-22 18:45:25 +0300

    Make decoder instances val. Add more instances

--- modules/core/src/main/scala/ru/tinkoff/phobos/decoding/ValueDecoder.scala
+++
@@ -1,7 +0,0 @@
-package ru.tinkoff.phobos.decoding
-
-trait ValueDecoder[A] {
-  def decode(history: List[String], value: String): Either[DecodingError, A]
-}
-
-object ValueDecoder extends ValueDecoderInstances
