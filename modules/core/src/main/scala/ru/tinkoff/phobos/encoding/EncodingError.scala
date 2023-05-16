b66fa641047f7e072e6ec07425fb98309eaa5e96
susliko <1istoobig@gmail.com>
2023-05-16 22:59:26 +0300

    Fix EncodingError message

--- modules/core/src/main/scala/ru/tinkoff/phobos/encoding/EncodingError.scala
+++ modules/core/src/main/scala/ru/tinkoff/phobos/encoding/EncodingError.scala
@@ -1,5 +1,5 @@
 package ru.tinkoff.phobos.encoding
 
 case class EncodingError(text: String, cause: Option[Throwable] = None) extends Exception(text, cause.orNull) {
-  override def getMessage: String = s"Error while decoding XML: $text"
+  override def getMessage: String = s"Error while encoding XML: $text"
 }
